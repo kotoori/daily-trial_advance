45fdaf83087d29d613c9251fb217cb69440a79f0
kotoori <kotoori0815@gmail.com>
2023-09-01 17:46:27 +0900

    Day01：phpinfo()を実行

--- PHP/index.php
+++ PHP/index.php
@@ -8,21 +8,21 @@
 <body>
   <h1>
     <?php
-        echo 1 + 2;
-        echo '<br>';
-        echo 4 - 1;
-        echo '<br>';
-        echo 12 * 12;
-        echo '<br>';
-        echo 12 / 3;
-        echo '<br>';
-        echo 12 / 3 + 1;
-        echo '<br>';
-        echo 12 / (3 + 1);
-        echo '<br>';
-        echo 10 % 3;
-
-?>
+      echo 1 + 2;
+      echo '<br>';
+      echo 4 - 1;
+      echo '<br>';
+      echo 12 * 12;
+      echo '<br>';
+      echo 12 / 3;
+      echo '<br>';
+      echo 12 / 3 + 1;
+      echo '<br>';
+      echo 12 / (3 + 1);
+      echo '<br>';
+      echo 10 % 3;
+      phpinfo();
+    ?>
   </h1>
 </body>
 </html>
