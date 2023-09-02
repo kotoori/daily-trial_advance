24f06f6ec856cfc1b9d50e21ff2bf172a3ecce0a
kotoori <kotoori0815@gmail.com>
2023-09-03 02:25:31 +0900

    Day04：連想配列を別ファイル(products.php)に分ける。 index.phpからrequire_onceでproducts.phpを読み込む。

--- Day04_EC/index.php
+++ Day04_EC/index.php
@@ -1,28 +1,4 @@
-<?php
-  $products = [
-    [
-      "name" => "アボカド",
-      "price" => 230,
-      "image" => "./images/image_1.jpg",
-    ],
-    [
-      "name" => "みかん",
-      "price" => 80,
-      "image" => "./images/image_2.jpg",
-    ],
-    [
-      "name" => "レモン",
-      "price" => 130,
-      "image" => "./images/image_3.jpg",
-    ],
-    [
-      "name" => "パイナップル",
-      "price" => 450,
-      "image" => "./images/image_4.jpg",
-    ],
-  ];
-?>
-
+<?php require_once 'products.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
---
+++ Day04_EC/products.php
@@ -0,0 +1,24 @@
+<?php
+  $products = [
+    [
+      "name" => "アボカド",
+      "price" => 230,
+      "image" => "./images/image_1.jpg",
+    ],
+    [
+      "name" => "みかん",
+      "price" => 80,
+      "image" => "./images/image_2.jpg",
+    ],
+    [
+      "name" => "レモン",
+      "price" => 130,
+      "image" => "./images/image_3.jpg",
+    ],
+    [
+      "name" => "パイナップル",
+      "price" => 450,
+      "image" => "./images/image_4.jpg",
+    ],
+  ];
+?>
