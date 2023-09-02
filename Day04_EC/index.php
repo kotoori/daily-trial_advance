e5f2a88e022369eb5a419b3575bd65a2cc837728
kotoori <kotoori0815@gmail.com>
2023-09-03 01:59:33 +0900

    Day04：phpで連想配列を記述 html内の直値を連想配列に置き換え カードを4つ作る

--- Day04_EC/index.php
+++ Day04_EC/index.php
@@ -1,3 +1,26 @@
+<?php
+  $productA = [
+    "name" => "アボカド",
+    "price" => 230,
+    "image" => "./images/image_1.jpg",
+  ];
+  $productB = [
+    "name" => "みかん",
+    "price" => 80,
+    "image" => "./images/image_2.jpg",
+  ];
+  $productC = [
+    "name" => "レモン",
+    "price" => 160,
+    "image" => "./images/image_3.jpg",
+  ];
+  $productD = [
+    "name" => "パイナップル",
+    "price" => 450,
+    "image" => "./images/image_4.jpg",
+  ];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,11 +34,71 @@
     <div class="app-container">
       <h1 class="title">DailyTrial Shopping</h1>
       <div class="cards-container">
+        <!-- card A-->
         <div class="card">
-          <img class="card-image" src="https://dnbz0c2oupsw6.cloudfront.net/bcekt8ctzrsfdj1gsus49v9tnhqu" alt="">
-          <p class="card-title">ミネラルウォーター</p>
+          <img class="card-image" src="<?php echo $productA["image"]; ?>" alt="">
+          <p class="card-title">
+            <?php
+              echo $productA["name"];
+            ?>
+          </p>
           <div class="flex justify-between">
-            <p class="card-price">230円</p>
+            <p class="card-price">
+              <?php
+                echo $productA["price"]."円";
+              ?>
+            </p>
+            <input min="0" class="item-number" type="number" value="0">
+          </div>
+        </div>
+        <!-- card B-->
+        <div class="card">
+          <img class="card-image" src="<?php echo $productB["image"]; ?>" alt="">
+          <p class="card-title">
+            <?php
+              echo $productB["name"];
+            ?>
+          </p>
+          <div class="flex justify-between">
+            <p class="card-price">
+              <?php
+                echo $productB["price"]."円";
+              ?>
+            </p>
+            <input min="0" class="item-number" type="number" value="0">
+          </div>
+        </div>
+        <!-- card C-->
+        <div class="card">
+          <img class="card-image" src="<?php echo $productC["image"]; ?>" alt="">
+          <p class="card-title">
+            <?php
+              echo $productC["name"];
+            ?>
+          </p>
+          <div class="flex justify-between">
+            <p class="card-price">
+              <?php
+                echo $productC["price"]."円";
+              ?>
+            </p>
+            <input min="0" class="item-number" type="number" value="0">
+          </div>
+        </div>
+        <!-- card D-->
+        <div class="card">
+          <img class="card-image" src="<?php echo $productD["image"]; ?>" alt="">
+          <p class="card-title">
+            <?php
+              echo $productD["name"];
+            ?>
+          </p>
+          <div class="flex justify-between">
+            <p class="card-price">
+              <?php
+                echo $productD["price"]."円";
+              ?>
+            </p>
             <input min="0" class="item-number" type="number" value="0">
           </div>
         </div>
