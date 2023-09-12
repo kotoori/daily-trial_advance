905e827e0fd5f58e19d8cd8e57aecd5b8d4ed4f5
kotoori <kotoori0815@gmail.com>
2023-09-12 23:39:11 +0900

    Day09：Wordpress標準機能を有効にする

--- Day07_wordpress/dev/wp-content/themes/TF30/functions.php
+++ Day07_wordpress/dev/wp-content/themes/TF30/functions.php
@@ -1,4 +1,12 @@
 <?php
+  function my_setup (){
+    add_theme_support('post-thumbnails');
+    add_theme_support('automatic-feed-links');
+    add_theme_support('title-tag');
+    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ) );
+  }
+  add_action("after_setup_theme", "my_setup");
+
   function my_script_init(){
     wp_enqueue_style( "font-awesome", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css", array(), "5.8.2", "all" );
     wp_enqueue_style( "my", get_template_directory_uri()."/css/style.css", array(), filemtime(get_theme_file_path('css/style.css')), "all" );
