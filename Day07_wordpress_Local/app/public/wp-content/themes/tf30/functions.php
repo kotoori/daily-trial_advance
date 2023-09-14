6bd713b137b77d954870742c354bade0bc6618a4
kotoori <kotoori0815@gmail.com>
2023-09-15 01:27:27 +0900

    functions.phpで定義した関数の位置を変更（処理内容は変更なし）

--- Day07_wordpress_Local/app/public/wp-content/themes/tf30/functions.php
+++ Day07_wordpress_Local/app/public/wp-content/themes/tf30/functions.php
@@ -1,13 +1,4 @@
 <?php
-  /* CSS/JSの読み込み */
-  function my_script_init(){
-    wp_enqueue_style("font-awesome", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css", array(), "5.8.2", "all");
-    wp_enqueue_style("my", get_template_directory_uri()."/css/style.css", array(), filemtime(get_theme_file_path('css/style.css')), "all");
-    wp_enqueue_script("my", get_template_directory_uri()."/js/script.js", array("jquery"), filemtime(get_theme_file_path('/js/script.js')), true);
-  }
-
-  add_action("wp_enqueue_scripts", "my_script_init");
-
   /* wordpress標準機能の拡張 */
   function my_setup(){
     add_theme_support('post-thumbnails'); /* アイキャッチ画像の設定 */
@@ -17,4 +8,12 @@
   }
 
   add_action("after_setup_theme", "my_setup");
-  
+    /* CSS/JSの読み込み */
+  function my_script_init(){
+    wp_enqueue_style("font-awesome", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css", array(), "5.8.2", "all");
+    wp_enqueue_style("my", get_template_directory_uri()."/css/style.css", array(), filemtime(get_theme_file_path('css/style.css')), "all");
+    wp_enqueue_script("my", get_template_directory_uri()."/js/script.js", array("jquery"), filemtime(get_theme_file_path('/js/script.js')), true);
+  }
+
+  add_action("wp_enqueue_scripts", "my_script_init");
+
